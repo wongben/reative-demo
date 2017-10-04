7134c93f744bcc5d44de1e72f6988f8b87a84861
weijun wang <weijun wang>
2017-10-04 11:50:59 +0800

    Initial commit

---
+++ src/main/java/wong/ben/ReativeDemoApplication.java
@@ -0,0 +1,12 @@
+package wong.ben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReativeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReativeDemoApplication.class, args);
+	}
+}
